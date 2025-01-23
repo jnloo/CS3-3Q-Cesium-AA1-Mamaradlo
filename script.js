961041792b534547f2881b296886281ff73396b1
Jhielou Marie <jmsmamaradlo22@src.pshs.edu.ph>
2025-01-23 08:37:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
+// Constant and Variables
 const PI = 3.14;
 var radius = 6.378e6;
 
+// Calculations
 let circumference = 2*PI*radius;
 let surfaceArea = 4*PI*(radius)**2;
 let volume = (4/3)*PI*(radius)**3;
-	
+
+// Final output
 document.write('The radius of the sphere is ' + radius + '<br>');
 document.write('The circumference is ' + circumference + '<br>');
 document.write('The surface area is ' + surfaceArea + '<br>');
 document.write('The volume is ' + volume + '<br>');
-	
-	
-
-	
